977f48a7ece45298bf157ed5047916358fe206aa
Your Name <you@example.com>
2020-05-04 21:35:09 +0200

    vip

--- objects/paddle/paddle.cs
+++ objects/paddle/paddle.cs
@@ -3,48 +3,48 @@ using System;
 
 public class paddle : KinematicBody2D
 {
-    [Export] private float angleEnd = -30;
-    [Export] private float time = 3;
+	[Export] private float angleEnd = -30;
+	[Export] private float time = 3;
 
-    private bool pressed = false;
-    private float angleStart;
-    Tween tween;
-
-
-    public override void _Ready()
-    {
-        angleStart = RotationDegrees;
-        tween = GetNode("Tween") as Tween;
-    }
+	private bool pressed = false;
+	private float angleStart;
+	Tween tween;
 
 
-    public void Activate()
-    {
-        if (pressed)
-        {
-            pressed = false;
-            Released();
-        } else {
-            pressed = true;
-            Pressed();
-        }
-    }
+	public override void _Ready()
+	{
+		angleStart = RotationDegrees;
+		tween = GetNode("Tween") as Tween;
+	}
 
 
-    private void Pressed()
-    {
-        tween.Stop(this);
-        float timeTo = time / Mathf.Abs(RotationDegrees - angleEnd);
-        tween.InterpolateProperty(this, "rotation_degrees", RotationDegrees, angleEnd, timeTo);
-        tween.Start();
-    }
+	public void Activate()
+	{
+		if (pressed)
+		{
+			pressed = false;
+			Released();
+		} else {
+			pressed = true;
+			Pressed();
+		}
+	}
 
 
-    private void Released()
-    {
-        tween.Stop(this);
-        float timeTo = time / Mathf.Abs(RotationDegrees - angleStart);
-        tween.InterpolateProperty(this, "rotation_degrees", RotationDegrees, angleStart, timeTo);
-        tween.Start();
-    }
+	private void Pressed()
+	{
+		tween.Stop(this);
+		float timeTo = time / Mathf.Abs(RotationDegrees - angleEnd);
+		tween.InterpolateProperty(this, "rotation_degrees", RotationDegrees, angleEnd, timeTo);
+		tween.Start();
+	}
+
+
+	private void Released()
+	{
+		tween.Stop(this);
+		float timeTo = time / Mathf.Abs(RotationDegrees - angleStart);
+		tween.InterpolateProperty(this, "rotation_degrees", RotationDegrees, angleStart, timeTo);
+		tween.Start();
+	}
 }

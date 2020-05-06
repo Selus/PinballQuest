82b0b673771db5cab7a0d3b3840b1ea52d343c66
Your Name <you@example.com>
2020-05-06 19:38:18 +0200

    asjdaklsdja

--- Main.cs
+++ Main.cs
@@ -7,60 +7,27 @@ public class Main : Node2D
 	private int points = 0;
 	private BallCamera camera;
 	private List<Ball> balls = new List<Ball>();
-	private List<Level> checkpoints = new List<Level>();
-	private Level currentCheckpoint;
+	private List<Checkpoint> checkpoints = new List<Checkpoint>();
+	private Checkpoint currentCheckpoint;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		instance = this;
 		camera = GetNode("BallCamera") as BallCamera;
 		GetTree().Paused = true;
-		generateMap();
-	}
-
-	private void generateMap()
-	{
-		var scene = (PackedScene)ResourceLoader.Load("res://levels/Level_0.tscn");
-		var node = (Level)scene.Instance();
-		node.PauseMode = Node.PauseModeEnum.Stop;
-		AddChild(node);
-		if(node.CheckPoint)
+		var children = GetNode("Level_3").GetChildren();
+		foreach (Node child in children)
 		{
-			camera.LimitBottom = (int)(node.Position.y + node.Height);
-			currentCheckpoint = node;
+			if (child.GetType() == typeof(Checkpoint))
+			{
+				checkpoints.Add(child as Checkpoint);
+			}
+			if (child.GetType() == typeof(Ball))
+			{
+				balls.Add(child as Ball);
+			}
 		}
-		Level lastNode;
-		var scene2 = (PackedScene)ResourceLoader.Load("res://levels/Level_1.tscn");
-		var node2 = (Level)scene2.Instance();
-		lastNode = node2;
-		node2.Position = new Vector2(node.Position.x,node.Position.y-node2.Height);
-		node2.PauseMode = Node.PauseModeEnum.Stop;
-		AddChild(node2);
-		scene2 = (PackedScene)ResourceLoader.Load("res://levels/Level_2.tscn");
-		node2 = (Level)scene2.Instance();
-		node2.Position = new Vector2(lastNode.Position.x,lastNode.Position.y-node2.Height);
-		node2.PauseMode = Node.PauseModeEnum.Stop;
-		AddChild(node2);
-		if(node.CheckPoint)
-		{
-			checkpoints.Add(node2);
-		}
-		lastNode = node2;
-		for( int i = 0; i < 10 ; i++) 
-		{
-			scene2 = (PackedScene)ResourceLoader.Load("res://levels/Level_1.tscn");
-			node2 = (Level)scene2.Instance();
-			node2.Position = new Vector2(lastNode.Position.x,lastNode.Position.y-node2.Height);
-			node2.PauseMode = Node.PauseModeEnum.Stop;
-			AddChild(node2);
-			lastNode = node2;
-		}
-		scene = (PackedScene)ResourceLoader.Load("res://objects/ball/Ball.tscn");
-		var nodeBall = (Node2D)scene.Instance();
-		nodeBall.PauseMode = Node.PauseModeEnum.Stop;
-		nodeBall.Position = new Vector2(300,-500);
-		AddChild(nodeBall);
-		balls.Add((Ball)nodeBall);
+		// generateMap();
 	}
 
 	public override void _Process(float delta)
@@ -68,16 +35,18 @@ public class Main : Node2D
 		//ball tracker
 		foreach(Ball ball in balls)
 		{
-			GD.Print(checkpoints.Count);
 		 	if(checkpoints.Count != 0)
 		 	{
-		 		var checkpoint = checkpoints[0];
-		 		if(ball.Position.y < checkpoint.Position.y)
-		 		{
-		 			currentCheckpoint = checkpoint;
-		 			checkpoints.Remove(checkpoint);
-		 			camera.LimitBottom = (int)(checkpoint.Position.y + checkpoint.Height);
-		 		}
+		 		for(int i = 0;i < checkpoints.Count ; i++)
+				{
+					var checkpoint = checkpoints[i];
+					if(ball.Position.y < checkpoint.Position.y)
+					{
+						currentCheckpoint = checkpoint;
+						checkpoints.Remove(checkpoint);
+						camera.LimitBottom = (int)(checkpoint.Position.y);
+					}
+				}
 		 	}
 		}
 
